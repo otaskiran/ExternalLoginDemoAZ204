84c7c94637355d20efff423b2cfc0797bcae0fc5
Osman Taskiran <developer.otaskiran@gmail.com>
2021-03-31 15:36:11 +0300

    first commit

---
+++ ExternalLoginDemoAz204/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ExternalLoginDemoAz204.Startup))]
+namespace ExternalLoginDemoAz204
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
